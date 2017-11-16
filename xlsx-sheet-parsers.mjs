a652ae5dd54146758bdb5d90604d5e37e4c0079b
root <morkeltry@mailfence.com>
2017-11-16 15:53:54 +0000

    Identifies headers and data start cell in sheets with multiple header lines and multiple row headings

--- xlsx-sheet-parsers.mjs
+++ xlsx-sheet-parsers.mjs
@@ -26,12 +26,12 @@ const incX = (cell, amount) => {
   xNum += (amount || 1);
 
   if (xNum >702)
-    throw new Error ("More than 702 columns? You mad?");
+    throw new Error ("From "+cell+" - More than 702 columns? You mad?");
   // Don't throw if decrementing to/below zero - return null so we can use it in loops.
   if (xNum <1)
     return null;
 
-  x = xNum>26? String.fromCharCode(64+Math.floor(xNum/26))
+  x = xNum>26? String.fromCharCode(64+Math.floor((xNum-1)/26))
    : '';
   x += String.fromCharCode(65+(xNum-1)%26);
 
@@ -52,8 +52,85 @@ const incY = (cell, amount) => {
 
 const onsWithRowHierarchy = sheet => {
 
-  console.log(Object.keys(sheet).slice(0,100).join(' # '));
-  console.log('one bit: ', sheet.D12.v);
+  console.log(Object.keys(sheet).slice(0,300).join(' # '));
+  console.log('accesing data example: ', sheet.D12.v);
+
+  const restOfRowEmpty = (startCell, stopAt) => {
+    // two methods to check this.
+    // If no stopAt provided, then just check if next cell in memory is same row.
+    // If stopAt provided, check for existence of specific cells.
+    if (!stopAt) {
+      const cells = Object.keys(sheet);
+      const idx = cells.indexOf(startCell);
+      if (idx == -1) {
+        // can't check the next in the list if startCell isn't in the list. See console.log
+        stopAt = incX (startCell, 5)
+        // console.log(`Empty cell ${startCell} passed for checking. I'll assume a stopAt value of ${stopAt}.`);
+      } else {
+
+        // if startCell last in sheet list then the rest of row must be empty.
+        if (cells.length==idx+1)
+          return true;
+        const nextCell = cells[idx+1];
+        // if next item matches row number, rest of row is not empty, if no match, it should be empty.
+        return !(startCell.match(splitterRegex)[2] === nextCell.match(splitterRegex)[2])
+      }
+    }
+      // allow stopAt to be an integer which will be added to startCell
+      if (Number.isInteger(stopAt) && stopAt>0)
+        stopAt = incX(startCell, stopAt);
+
+    if (startCell.match(splitterRegex)[2] != stopAt.match(splitterRegex)[2])
+      throw new Error (`Comparing ${startCell} and ${stopAt} but they are different rows.`)
+
+    // Traverse along until startCell== topAt or until non-empty cell found
+    while (startCell != stopAt) {
+      if (sheet[startCell])
+        return false;
+    startCell= incX(startCell);
+    }
+    return true;
+  };
+
+  let current = 'A1';
+  let dataStart = '';           // Best guess for top left data cell, see comment below
+  let currentHeader = '';
+  let headersStart = [];        // Array of: the first cell in a row which contains a header
+
+  // Traverse downwards until you find a row with something in more than just the first cell.
+  while (restOfRowEmpty(current))
+    current = incY (current);
+  // So long as this non-empty row is empty in this first cell,
+  while (!sheet[current]) {
+    currentHeader = current;
+    // traverse to the right to the first non-empty cell
+    while (!sheet[currentHeader]){
+      currentHeader = incX (currentHeader);
+    }
+    // and remember it.
+    headersStart.push (currentHeader);
+    // this is a while, not an if, since there may be more than one header row
+    // so repeat for the next row if also it's first cell is empty.
+    current = incY (current);
+  }
+
+  // assuming that the first header row we found contains some header in the first data column.
+  dataStart = headersStart[0].match(splitterRegex)[1] + current.match(splitterRegex)[2];
+
+  console.log ('I reckons the headers are:');
+  headersStart.forEach (header => {
+    while (!restOfRowEmpty(header, 50)) {
+      console.log(sheet[header].v);
+      // console.log(header);
+      header = incX (header);
+      while (!sheet[header] && !restOfRowEmpty(header, 50))
+        header = incX (header);
+    }
+    console.log('OK then');
+  })
+  console.log('and the data starts at cell ',dataStart);
+
+
 
 };
 
