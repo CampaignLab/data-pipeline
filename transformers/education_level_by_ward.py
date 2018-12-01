4bbedfae2f3cbcfe2ef8fc8d15a55e8e73758309
monkeybrie <morkeltry@mailfence.com>
2018-12-01 18:46:22 +0000

    calling from command line with GSS codes prints the grabbed rows

--- transformers/education_level_by_ward.py
+++ transformers/education_level_by_ward.py
@@ -4,16 +4,25 @@
 """row_by_GSS(GSScode) returns a full row """
 
 import pandas as pd
+import argparse
 import by_ward_lookup as maker
 
 
 def main():
-    print (row_by_GSS('E36000091'))
-    print (row_by_GSS('95ZZ16'))
+    parser = argparse.ArgumentParser(description='Output a row')
+    parser.add_argument('codes', metavar='GSScodes', type=str,
+                        nargs='*', default=['E36000091','95ZZ16'],
+                        help='WD11CD ward code/s (may be 95xxxxx for N.I.)')
+
+    args = parser.parse_args()
+    for code in args.codes:
+        print (code+':')
+        print (row_by_GSS(code))
 
 
 edlevel = pd.read_csv('./education_level.csv')
 
+### This grabs the row
 """row_by_GSS(GSScode) is a function that returns a full row as pandas DataFrame """
 row_by_GSS = maker.make_row_by_GSS(edlevel)
 
